48ad5b2161a23fd3a3fbdd7b1b5e3f82a878522c
sanlley747-star <sanlley747@gmail.com>
2026-09-26 12:38:43 -0400

    Bloque 6: desincronizar cron de cuentas canal

---
+++ supabase/migrations/20260926192100_bloque_6_offsets_cron_cuentas_canal.sql
@@ -0,0 +1,4 @@
+select cron.alter_job(3, schedule := '0,15,30,45 * * * *');
+select cron.alter_job(4, schedule := '4,19,34,49 * * * *');
+select cron.alter_job(5, schedule := '8,23,38,53 * * * *');
+select cron.alter_job(11, schedule := '12,27,42,57 * * * *');
